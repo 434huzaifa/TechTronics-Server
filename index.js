5b7cb401942f7736ae6d7f764540dad87fec5458
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2023-10-20 23:31:41 +0600

    code cleanup

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json())
 
 const uri = "mongodb+srv://434darkmaster:kCnhj5usUf3mXiDF@cluster0.l2bfny4.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,8 +20,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
     const database = client.db("Techtronics");
     const etCollection = database.collection("etCollection");
     const brandCollection = database.collection("brands");
@@ -145,12 +142,9 @@ async function run() {
     })
 
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+
   }
 }
 run().catch(console.dir);

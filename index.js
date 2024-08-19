d3cf08ea8b22b988de22786c520275d497914f59
H <saadhuzaifa2497@gmail.com>
2024-08-20 05:26:05 +0600

    database fixed

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
+const dotenv=require("dotenv")
+dotenv.config()
 const app = express()
-
 const port=process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json())
 
-const uri = "mongodb+srv://<MONGODB_USERNAME>:<MONGODB_PASSWORD>@cluster0.l2bfny4.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.l2bfny4.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {

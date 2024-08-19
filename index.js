9e12dddc877043805a677bce8834184fc53a38bb
H <saadhuzaifa2497@gmail.com>
2024-08-20 05:30:28 +0600

    smalll changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ dotenv.config()
 const app = express()
 const port=process.env.PORT || 5000
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.l2bfny4.mongodb.net/?retryWrites=true&w=majority`;
